47fcdef4278232ec71a50abb9d40ec5497b75c99
Evan Chernicky <echernicky@gmail.com>
2025-09-28 18:36:34 -0400

    Remove build folder from repo

--- build/blocks/worklistings/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data'), 'version' => '5d7c53faba6b3607ea27');
--- build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '6536d58abdacd7c8ede4');
