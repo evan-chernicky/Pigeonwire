b8fdf0147b50f62643570665c09b6289d6d7e23e
Evan Chernicky <echernicky@gmail.com>
2025-10-10 17:03:31 -0400

    completed video background custom block

--- src/blocks/videobackground/edit.js
+++ src/blocks/videobackground/edit.js
@@ -1,48 +1,138 @@
-import { useBlockProps, MediaUpload, MediaUploadCheck, InspectorControls, InnerBlocks } from "@wordpress/block-editor"
-import {Button, PanelBody, PanelRow} from '@wordpress/components'
+import {useState, useEffect} from "@wordpress/element"
+import { useBlockProps, MediaUpload, MediaUploadCheck, InspectorControls, InnerBlocks, MediaPlaceholder } from "@wordpress/block-editor"
+import {Button, PanelBody, PanelRow, DropZone, BaseControl, Spinner } from '@wordpress/components'
+import { useSelect } from '@wordpress/data';
+import { store as blockEditorStore } from '@wordpress/block-editor';
 
 
 export default function Edit({ attributes, setAttributes }) {
     const blockProps = useBlockProps()
-    const { videoID, videoURL } = attributes
+    const { videoID, videoURL, fileName, posterID } = attributes
+    const [localPreview, setLocalPreview] = useState(null);
 
+    //Fetching poster image
+    const posterImage = useSelect(
+        (select) => posterID ? select('core').getMedia(posterID) : null,
+        [posterID]
+    );
+
+    useEffect(() => {
+        if (posterID && localPreview) {
+            URL.revokeObjectURL(localPreview)
+            setLocalPreview(null);
+        }
+    },[posterID])
+
+    //We just want to preview the medium sized image if we can
+    const mediumPosterURL = posterImage?.media_details?.sizes?.medium?.source_url || posterImage?.source_url || null;
     
+    //Attempt to show uploaded URL, else local uploaded into drop area, else nothing
+    const backgroundURL = localPreview || mediumPosterURL  || '';
+
+    // Retrieves Gutenberg's internal file upload function for handling  media uploads.
+    const mediaFileUpload = useSelect((select) => select(blockEditorStore).getSettings().mediaUpload, []);
+
+    function onFileDrop(files) {
+        if (!files.length) return;
+        
+        const file = files[0];
+        const previewURL = URL.createObjectURL(file); // temporary local preview
+        setLocalPreview(previewURL);
+
+        mediaFileUpload({
+            filesList: files,
+            allowedTypes: ['image'],
+            onFileChange: ([image]) => {
+                onPosterSelect(image);
+            },
+            onError: (err) => {
+                console.error('Media upload failed', err);
+            }
+        });
+    }
+
+    function onPosterSelect(image) {
+        setAttributes({ posterURL: image.url, posterID: image.id })
+    }
+
+    function onVideoSelect(video) {
+        setAttributes({ videoID: video.id, videoURL: video.url, fileName: video.filename })
+    }
+
     return (
         <div {...blockProps}>
-            {!videoURL && (
-                <MediaUploadCheck>
-                    <MediaUpload
-                        onSelect={(video) => setAttributes({ videoID: video.id, videoURL: video.url })}
-                        allowedTypes={['video']} // Limit to certain types
-                        value={videoID}
-                        render={({ open }) => (
-                            <Button onClick={open}>
-                                {videoURL ? 'Change Video' : 'Select Video'}
-                            </Button>
-                        )}
-                    />
-                </MediaUploadCheck>
-            )}
             <InspectorControls>
                 <PanelBody title="Background" initialOpen={true}>
                     <PanelRow>
+                        <BaseControl.VisualLabel>Background Video</BaseControl.VisualLabel>
+                    </PanelRow>
+                    <PanelRow>
                         <MediaUploadCheck>
-                        <MediaUpload 
-                            onSelect={(video) => setAttributes({ videoID: video.id, videoURL: video.url })} 
-                            value={videoID} render={({open}) => {
-                                return <Button onClick={open}>{videoURL ? 'Change Video' : 'Select Video'}</Button>
-                            }} 
-                        />
+                            <MediaUpload 
+                                onSelect={video => onVideoSelect(video)}
+                                allowedTypes={['video/mp4']}
+                                value={videoID} 
+                                render={({open}) => {
+                                    return <Button variant="secondary" onClick={open}>{videoURL ? 'Change Video' : 'Select Video'}</Button>
+                                }} 
+                            />
                         </MediaUploadCheck>
+                        {videoURL && <span style={{ marginLeft: '10px' }}>{fileName}</span>}
+                    </PanelRow>
+                    <div  style={{marginBottom: '20px'}}></div>
+                    <PanelRow>
+                        <BaseControl.VisualLabel>Background Poster</BaseControl.VisualLabel>
+                    </PanelRow>
+                    <PanelRow>
+                        <div className="poster-dropzone" style={{backgroundImage: `url("${backgroundURL || ''}")`}}>
+                            <DropZone
+                                onFilesDrop={ files => onFileDrop(files) }
+                                onHTMLDrop={ files => onFileDrop(files)  }
+                            />
+                            <MediaUploadCheck>
+                                <MediaUpload
+                                    onSelect={image => onPosterSelect(image)}
+                                    allowedTypes={['image']}
+                                    value={posterID}
+                                    render={({ open }) => (
+                                        <>
+                                            { localPreview ? (
+                                                <Spinner />
+                                            ) : (
+                                                <Button variant="primary" onClick={open}>
+                                                    {posterID ? 'Change Poster' : 'Select Poster'}
+                                                </Button>
+                                            )}
+                                        </>
+                                    )}
+                                />
+                            </MediaUploadCheck>
+                        </div>
                     </PanelRow>
                 </PanelBody>
             </InspectorControls>
-            <div className="video-background">
-                <div className="video-background__content-container">
-                    <InnerBlocks renderAppender={InnerBlocks.ButtonBlockAppender}/>
+            {!videoURL ? (
+                <MediaPlaceholder
+                    onSelect={ (video) => {
+                    setAttributes({
+                        videoID: video.id,
+                        videoURL: video.url,
+                    });
+                    } }
+                    allowedTypes={ ['video'] }
+                    multiple={ false }
+                    labels={ { title: 'Background Video' } }
+                    accept="video/mp4"
+                    disableDropZone={ false }
+                />
+            ) : (
+                <div className="video-background">
+                    <div className="video-background__content-container">
+                        <InnerBlocks renderAppender={InnerBlocks.ButtonBlockAppender}/>
+                    </div>
+                    {videoURL && <video src={videoURL} style={{ maxWidth: '100%' }} muted autoPlay loop />}
                 </div>
-                {videoURL && <video src={videoURL} style={{ maxWidth: '100%' }} muted autoPlay loop />}
-            </div>
+            ) }
         </div>
     )
 }
--- src/blocks/videobackground/render.php
+++ src/blocks/videobackground/render.php
@@ -1,2 +1,15 @@
-Hello from render.php
 <?php
+$videoURL = $attributes['videoURL'] ?? null;
+$poster = $attributes['posterURL'] ?? '';
+
+if ($videoURL) {?>
+    <div class="video-background">
+        <div class="video-background__content-container">
+            <?php echo $content; ?>
+        </div>
+        <video style="max-width: 100%;" muted autoPlay loop poster="<?php echo esc_url($poster); ?>">
+              <source src="<?php echo esc_url($videoURL); ?>" type="video/mp4" />
+                Your browser does not support HTML5 video.
+        </video>
+    </div>
+<?php } ?>
