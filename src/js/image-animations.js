28216058701d2b723f10fc7ffa66441e8ac57f8e
Evan Chernicky <echernicky@gmail.com>
2025-10-15 19:25:44 -0400

    responsive edits

--- src/js/image-animations.js
+++ src/js/image-animations.js
@@ -1,13 +1,13 @@
 import gsap from 'gsap'
 
-gsap.from(".scale-on-scroll", {
-  scale: 1.1, 
-  scrollTrigger: {
-    trigger: ".scale-on-scroll",
-    start: "top bottom", 
-    end: "center center", 
-    scrub: 0.6,   
-    // markers: true
-  }
+gsap.utils.toArray(".scale-on-scroll").forEach(el => {
+  gsap.from(el, {
+    scale: 1.1,
+    scrollTrigger: {
+      trigger: el,
+      start: "top bottom",
+      end: "center center",
+      scrub: 0.6,
+    }
+  });
 });
-
