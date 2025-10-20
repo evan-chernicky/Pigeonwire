1f92a4809e5657801863b16640d81b4f2ae73339
Evan Chernicky <echernicky@gmail.com>
2025-10-20 18:00:57 -0400

    prevents user from scrolling when mobile menu open and adds animation to menu items

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,12 @@
+import { gsap } from "gsap";
+import {smoother} from './smoothscroll'
+
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');
     const menuToggle = header?.querySelector('.mobile-menu-toggle');
+    const menuNavLinks = document.querySelectorAll(".mobile-menu .wp-block-navigation-item__content");
     let lastScrollY = window.scrollY;
+    let isMobileMenuToggled = false;
 
     //Hides header when the user scrolls down from the hero, but makes it reappear if user starts to scroll back up
     function onScroll() {
@@ -24,6 +29,22 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     window.addEventListener('scroll', () => onScroll());
-    menuToggle.addEventListener('click', () => header.classList.toggle('mobile-menu-isOpen'))
-});
 
+    menuToggle.addEventListener('click', () => {
+        isMobileMenuToggled = !isMobileMenuToggled
+        header.classList.toggle('mobile-menu-isOpen')
+
+        if (isMobileMenuToggled) {
+            smoother.paused(true)
+            gsap.from(menuNavLinks, {
+                opacity: 0,          
+                y: 20,               
+                duration: 0.6,       
+                stagger: 0.2,        
+                ease: "power2.out"
+            });
+        } else {
+            smoother.paused(false)
+        }
+    })
+});
