878617cfd556bc8cc7a6968c016feca58c5bcc30
Evan Chernicky <echernicky@gmail.com>
2025-10-21 15:25:05 -0400

    adds safari bug adjustments

--- src/blocks/videobackground/render.php
+++ src/blocks/videobackground/render.php
@@ -7,7 +7,7 @@ if ($videoURL) {?>
         <div class="video-background__content-container">
             <?php echo $content; ?>
         </div>
-        <video style="max-width: 100%;" muted autoPlay loop poster="<?php echo esc_url($poster); ?>">
+        <video style="max-width: 100%;" muted autoPlay loop playsinline webkit-playsinline poster="<?php echo esc_url($poster); ?>">
               <source src="<?php echo esc_url($videoURL); ?>" type="video/mp4" />
                 Your browser does not support HTML5 video.
         </video>
